151e48d716aa3063a4922bcb0c8705f8035e182e
sagar p <sagar.bs2008@gmail.com>
2022-01-04 23:42:10 +0530

    added test in junit

--- src/test/java/com/simpliLearn/jenkins/JenkinsApplicationTests.java
+++ src/test/java/com/simpliLearn/jenkins/JenkinsApplicationTests.java
@@ -1,6 +1,8 @@
 package com.simpliLearn.jenkins;
 
+
 import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
@@ -8,6 +10,16 @@ class JenkinsApplicationTests {
 
 	@Test
 	void contextLoads() {
+		assertTrue(true);	
 	}
+	@Test
+	void checkUrl() {
+		assertTrue(true);	
+	}
+	@Test
+	void checkNavigation() {
+		assertTrue(false);	
+	}
+
 
 }
